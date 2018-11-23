8ebd8794e52c1fd800e37b74b9c30063e9f15341
student <student@localhost>
2018-11-23 12:11:56 +0300

    initials support for onr-word names (e.g. "Vasiliy" -> "V")

--- src/test/java/ru/itpark/service/HelperServiceTest.java
+++ src/test/java/ru/itpark/service/HelperServiceTest.java
@@ -15,5 +15,13 @@ class HelperServiceTest {
 
             assertEquals(expected, actual);
         }
+
+        {
+            HelperService service = new HelperService();
+            String actual = service.initials("Vasiliy");
+            String expected = "V";
+
+            assertEquals(expected, actual);
+        }
     }
 }
